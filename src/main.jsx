6741f3301ed358f4ba80d77720e9ea72c4e0b870
Mohamed Amoussa <mohamedj81@hotmail.com>
2023-12-28 23:28:58 +0100

    Ajout des routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,45 @@
+import './App.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+import { Navbar } from './components/Navbar.jsx'
+import { Footer } from './components/Footer.jsx'
 import Home from './pages/Home.jsx'
-import './App.css'
+import About from './pages/About.jsx'
+import ErrorPage from './pages/ErrorPage.jsx'
+import { Housing } from './pages/Housing.jsx'
+
+
+const Layout = () => {
+  return (
+    <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+    </>
+    )
+}
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />
-  },
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: '/about',
+        element: <About />
+      },
+      {
+        path: '/housing',
+        element: <Housing />
+      },
+    ]
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
