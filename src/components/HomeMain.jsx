eb2a6f437c6686edba46d7dd9a59c48347de329e
Mohamed Amoussa <mohamedj81@hotmail.com>
2023-12-28 23:31:24 +0100

    Homepage: Restructuration du code

--- src/components/HomeMain.jsx
+++ src/components/HomeMain.jsx
@@ -4,31 +4,27 @@ import HomeMainFig from './HomeMainFig'
 
 export default function HomeMain() {
 
-    const fetchData = () => {
-        fetch("database.json")
-        .then(r => r.json())
-        .then(json => setHomeFigure(json))
-        .catch(console.error)
-      }
-    
-      const [homeFigure, setHomeFigure] = useState([]);
-    
-      useEffect(fetchData, []);
-    
-        const figures = homeFigure
-                    .map((data, index) => {
-                    if(index >= 1 && index < 8) {
-                      return <HomeMainFig
-                              key={data.id}
-                              id={data.id}
-                              title={data.title}
-                              cover={data.cover}/>
-                    }
-                    return null;
-                    })
-                    .slice(0, 7)
-    
+  const fetchData = () => {
+    fetch("database.json")
+    .then(r => r.json())
+    .then(json => setData(json))
+    .catch(console.error)
+  }
+
+  const [data, setData] = useState([]);
+
+  useEffect(fetchData, []);
+
+  const thumbs = data.slice(0, 6)
+              .map((data) => {
+                return <HomeMainFig
+                        key={data.id}
+                        id={data.id}
+                        title={data.title}
+                        cover={data.cover}/>
+              })
+
   return (
-    <main className='main'>{figures}</main>
+    <main className='main'>{thumbs}</main>
   )
 }
--- src/components/HomeMainFig.jsx
+++ src/components/HomeMainFig.jsx
@@ -2,17 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function HomeMainFig(props) {
+
+  const Figure = () => {
+    return (
+      <figure className="main__housing">
+              <Link
+              className='main__housing__link'
+              to="/housing">
+                  <img src={props.cover} alt={props.title} className='main__housing__link__img' />
+                  <figcaption className="main__housing__link__title">
+                      {props.title}
+                  </figcaption>
+              </Link>
+      </figure>
+    )
+  }
+
   return (
-    <figure className="main__housing">
-            <Link
-            className='main__housing__link'
-            to="/">
-                <img src={props.cover} alt={props.title} className='main__housing__link__img' />
-                <figcaption className="main__housing__link__title">
-                    {props.title}
-                </figcaption>
-            </Link>
-    </figure>
+    <Figure />
   )
 }
 
