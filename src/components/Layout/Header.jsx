b7258d72f4656d143830e7d00165e2785bedb767
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-01-02 00:02:08 +0100

    Création et intégration composants Header, Footer, et Layout

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export function Header() {
+
+    const NavbarLogo = () => {
+        return (
+            <NavLink to={"/"}
+                className={({ isActive }) => (isActive ? 'active' : 'inactive')}>
+                <img className="nav__logo" src="logo-nav.svg" alt="Kasa Logo" />
+            </NavLink>
+        )
+    }
+
+    const NavbarLinks = () => {
+        return (
+            <ul className="nav__list">
+                <li className="nav__list__home">
+                    <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to={"/"}>
+                        Accueil
+                    </NavLink>
+                </li>
+                <li className="nav__list__about">
+                    <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to={"/about"}>A Propos
+                    </NavLink>
+                </li>
+            </ul>
+        )
+    }
+
+    return (
+      <header>
+         <nav className="navbar">
+            <NavbarLogo />
+            <NavbarLinks />
+         </nav>
+      </header>
+    )
+}
