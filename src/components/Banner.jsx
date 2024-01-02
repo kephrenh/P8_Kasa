b327a726e5e138ab095ae79f04ee4b0eea0af984
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-01-02 19:50:28 +0100

    Banner: Modification titre du composant

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,10 @@ import React from "react"
 export default function Banner(props) {
     return (
         <div className="banner" style={{backgroundImage: `url(${props.bannerImage})`}}>
-            <h1>{props.bannerText}</h1>
+            <h1>
+               <span>{props.bannerTextLine1}</span>
+               <span>{props.bannerTextLine2}</span>
+            </h1>
         </div>
     )
 }
