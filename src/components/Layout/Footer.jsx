e240dabc80fa02ab6804cfeca14b367729b246a9
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-01-02 19:52:53 +0100

    Composant Footer: modification logo path

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export function Footer() {
   return (
     <footer className="footer">
-        <img src="logo-footer.svg" alt="Kasa Logo" className="footer__logo" />
+        <img src="src/assets/logo-footer.svg" alt="Kasa Logo" className="footer__logo" />
         <p className='footer__text'>© 2020 Kasa. All rights reserved</p>
     </footer>
   )
