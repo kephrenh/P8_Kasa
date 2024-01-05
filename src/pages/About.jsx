735a7f13c6fc33820c3161667e8624bf23865c48
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-01-05 16:19:30 +0100

    About Page: Intégration composant Collapse

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import Banner from '../components/Banner'
+import Collapse from '../components/Collapse'
 
 function About() {
+   document.title = "A Propos - Kasa"
 
   return (
-    <>
-    <h1>A propos</h1>
-    </>
+      <main className='about'>
+         <Banner
+            bannerImage='/src/assets/banner-about.png'
+         />
+         <div className='about__dropdowns'>
+            <Collapse
+               title="Fiabilité"
+               content='Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements et toutes les informations sont régulièrement vérifiées par nos équipes.'
+            />
+            <Collapse
+               title='Respect'
+               content='La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'
+            />
+            <Collapse
+               title='Service'
+               content='La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'
+            />
+            <Collapse
+               title='Sécurité'
+               content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique."
+            />
+         </div>
+      </main>
   )
 }
 
