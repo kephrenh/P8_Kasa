6cc6988099fb4496adbd93d1ff7f314219cb5835
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-01-05 16:05:45 +0100

    Error page: Corrections

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,20 +1,17 @@
 import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
+   document.title = "404 - Kasa"
 
     return (
         <main className="error">
             <h1 className="error__title" >404</h1>
             <p className="error__message">
-               <span>
-                  Oups! La page que 
-               </span>
-               <span>
-                  vous demandez n'existe pas.
-               </span>
+               <span>Oups! La page que </span>
+               <span>vous demandez n'existe pas.</span>
             </p>
             <Link to="/" className="error__link">
-                  Retourner sur la page d'accueils
+                  Retourner sur la page d'accueil
             </Link>
         </main>
     )
